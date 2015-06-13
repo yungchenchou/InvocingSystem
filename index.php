1131f82501cc8c5df0bb3155a9bf2756722befbb
yungchenchou <yungchen@gmail.com>
2015-06-13 16:40:36 +0800

    Change Title

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang=zh-TW>
+    <head>
+        <meta charset=utf-8>
+        <meta http-equiv=X-UA-Compatible content="IE=edge">
+        <meta name=viewport content="width=device-width, initial-scale=1">
+        <meta name=description content="HouseStyle">
+        <meta name=author content="Yung-Chen Chou">
+        <!-- <link rel=icon href=/Content/AssetsBS3/img/favicon.ico> -->
+        <title>House Style 衛浴用品館</title>
+        <link href=https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css rel=stylesheet>
+        <link href=css/dashboard.css rel=stylesheet>
+        <!--[if lt IE 9]>
+          <script src=~/Scripts/AssetsBS3/ie8-responsive-file-warning.js></script>
+        <![endif]-->
+        <script src=scripts/ie-emulation-modes-warning.js></script>
+        <!--[if lt IE 9]>
+          <script src=https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js></script>
+          <script src=https://oss.maxcdn.com/respond/1.4.2/respond.min.js></script>
+        <![endif]-->
+</head>
+<body>
+    <nav class="navbar navbar-inverse navbar-fixed-top" role=navigation>
+        <div class=container-fluid>
+            <div class=navbar-header>
+                <button type=button class="navbar-toggle collapsed" data-toggle=collapse data-target=#navbar aria-expanded=false aria-controls=navbar>
+                    <span class=sr-only>Toggle navigation</span>
+                    <span class=icon-bar></span>
+                    <span class=icon-bar></span>
+                    <span class=icon-bar></span>
+                </button>
+                <a class=navbar-brand href=#>Project name</a>
+            </div>
+            <div id=navbar class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href=#>Dashboard</a>
+                    <li><a href=#>Settings</a>
+                    <li><a href=#>Profile</a>
+                    <li><a href=#>Help</a>
+                </ul>
+                <form class="navbar-form navbar-right">
+                    <input class=form-control placeholder=Search...>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <div class=container-fluid>
+         <div class=row>
+             <div class="col-sm-3 col-md-2 sidebar">
+                 <ul class="nav nav-sidebar">
+                     <li class=active><a href=#>Overview <span class=sr-only>(current)</span></a>
+                     <li><a href=#>Reports</a>
+                     <li><a href=#>Analytics</a>
+                     <li><a href=#>Export</a>
+                 </ul>
+                 <ul class="nav nav-sidebar">
+                     <li><a href="">Nav item</a>
+                     <li><a href="">Nav item again</a>
+                     <li><a href="">One more nav</a>
+                     <li><a href="">Another nav item</a>
+                     <li><a href="">More navigation</a>
+                 </ul>
+                 <ul class="nav nav-sidebar">
+                     <li><a href="">Nav item again</a>
+                     <li><a href="">One more nav</a>
+                     <li><a href="">Another nav item</a>
+                 </ul>
+             </div>
+             <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+                 <h1 class=page-header>Dashboard</h1>
+                 <div class="row placeholders">
+                     <div class="col-xs-6 col-sm-3 placeholder">
+                         <img data-src=holder.js/200x200/auto/sky class=img-responsive alt="Generic placeholder thumbnail">
+                         <h4>Label</h4>
+                         <span class=text-muted>Something else</span>
+                     </div>
+                     <div class="col-xs-6 col-sm-3 placeholder">
+                         <img data-src=holder.js/200x200/auto/vine class=img-responsive alt="Generic placeholder thumbnail">
+                         <h4>Label</h4>
+                         <span class=text-muted>Something else</span>
+                     </div>
+                     <div class="col-xs-6 col-sm-3 placeholder">
+                         <img data-src=holder.js/200x200/auto/sky class=img-responsive alt="Generic placeholder thumbnail">
+                         <h4>Label</h4>
+                         <span class=text-muted>Something else</span>
+                     </div>
+                     <div class="col-xs-6 col-sm-3 placeholder">
+                         <img data-src=holder.js/200x200/auto/vine class=img-responsive alt="Generic placeholder thumbnail">
+                         <h4>Label</h4><span class=text-muted>Something else</span>
+                     </div>
+                 </div>
+                 <h2 class=sub-header>Section title</h2>
+                 <div class=table-responsive>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr><th>#<th>Header<th>Header<th>Header<th>Header
+                            <tbody>
+                                <tr><td>1,001<td>Lorem<td>ipsum<td>dolor<td>sit
+                                <tr><td>1,002<td>amet<td>consectetur<td>adipiscing<td>elit
+                                <tr><td>1,003<td>Integer<td>nec<td>odio<td>Praesent
+                                <tr><td>1,003<td>libero<td>Sed<td>cursus<td>ante
+                                <tr><td>1,004<td>dapibus<td>diam<td>Sed<td>nisi
+                                <tr><td>1,005<td>Nulla<td>quis<td>sem<td>at
+                                <tr><td>1,006<td>nibh<td>elementum<td>imperdiet<td>Duis
+                                <tr><td>1,007<td>sagittis<td>ipsum<td>Praesent<td>mauris
+                                <tr><td>1,008<td>Fusce<td>nec<td>tellus<td>sed
+                                <tr><td>1,009<td>augue<td>semper<td>porta<td>Mauris
+                                <tr><td>1,010<td>massa<td>Vestibulum<td>lacinia<td>arcu
+                                <tr><td>1,011<td>eget<td>nulla<td>Class<td>aptent
+                                <tr><td>1,012<td>taciti<td>sociosqu<td>ad<td>litora
+                                <tr><td>1,013<td>torquent<td>per<td>conubia<td>nostra
+                                <tr><td>1,014<td>per<td>inceptos<td>himenaeos<td>Curabitur
+                                <tr><td>1,015<td>sodales<td>ligula<td>in<td>libero
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src=https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js></script>
+    <script src=https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js></script>
+    <script src=scripts/docs.min.js></script>
+    <script src=scripts/ie10-viewport-bug-workaround.js></script>
+</body>
+</html>
